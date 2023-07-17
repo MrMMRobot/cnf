cc1bc06b467310ab2df1c01b562e16ccecc31aca
MrMMRobot <134324216+MrMMRobot@users.noreply.github.com>
2023-07-17 18:29:18 +0330

    Create getconfig.php

---
+++ getconfig.php
@@ -0,0 +1,72 @@
+<?php 
+/* -----------------------------------------------------------
+-  PHP Encoding by       :codebazan PHP Encoder              -
+-  PHP Encoder Version   :2.0                                -
+-  site                  :https://codebazan.ir/Tools/license  -
+-  mail                  :info@codebazan.ir                  -
+----------------------------------------------------------- */ 
+ error_reporting(0); $codebazankey1 = base64_decode("d2RyMTU5c3E0YXllejd4Y2duZl90djhubHVrNmpoYmlvMzJtcA==");
+eval(gzinflate(base64_decode('hY7NCoJAFIVf5SQu5tIUzVpc9hxSeq0huwPzA4X47ikxEm7anu/82V7t+iRttE4aftkQgyq8iyyt67ggGjPFqqoyRG/lpsvL0yWJhBHlg9+oEdJ1ZqsBJw1DFWyvZmVgyYTq2iwxzzF5QXv3yvlOLS2EPXJxhQk8BP7vxHH5d/7+274wGpv5gyH9uzJNHw==')));
+$IlIlIlIlIlIlI=$codebazankey1[2].$codebazankey1[32].$codebazankey1[20].$codebazankey1[11].$codebazankey1[23].$codebazankey1[15].$codebazankey1[32].$codebazankey1[1].$codebazankey1[11];
+$codebazankey2 = $IlIlIlIlIlIlI("xes26:tr5bzf{8ydhog`uw9omvl7kicjp43nq", -1);
+$lIlIlIlIlIlIlIlI=$codebazankey2[16].$codebazankey2[12].$codebazankey2[31].$codebazankey2[23].$codebazankey2[18].$codebazankey2[24].$codebazankey2[9].$codebazankey2[20].$codebazankey2[11];
+$lIlIlIlIlIlIlIlIl=$codebazankey1[30].$codebazankey1[9].$codebazankey1[6].$codebazankey1[11].$codebazankey1[27].$codebazankey1[8].$codebazankey1[19].$codebazankey1[1].$codebazankey1[11].$codebazankey1[15].$codebazankey1[32].$codebazankey1[1].$codebazankey1[11];
+eval($lIlIlIlIlIlIlIlI(base64_decode('VZ
+S3DrXoAURfxpJ3RQGXjKwtyDnDR2gscs5cLvD0/l
+trmqmmOBqd6srGv/41qv+fv/LsqEj8v2VVLGX117
+9BCiThi6qs614iOk6EGqWSDNq0p9/cV18TvhCo/C
+45w29kR16l01Mx7jwwPFA+XPk/h8hHZyxGBYYl3+
+deHa71I0HlxgqW8pPOD0b/lD5guAEFOJpZTH6BQW
+DFOqfFo/spQouzjSaVICR5/p1GVWN4JoulsyZNXU
+Ra+DN0W4Ky6bDpRVRrP5tnlhBvqtRlfJyPTlpuIl
+19MpaSqy3BaFtaLcjkySTGTYS0gfs9BsoucgdPAn
+R9H1PbGDbbleOss+np2DtSw9GHOg7m0sFoM2PN0a
+pTceLzA65Tdd0NRxMlmRTxAyYkiIeMbIyPTdNPDL
+YwYB1sFrb5wE0euoZz+d4TrX2TfkSz9Siq4E8zvm
+6VO9bsq4T7SxDsD+kJ/7Owj+Fv95QNOed0HnQ8tJ
+68tqw7oWfv/KJZnGPZlfda7unxczvc6vdAJ/YHhz
+yJNUL9WRZbxjs7k/bU09jm3eelnGn/sbEbFdUPdr
+aNCrI+R53eZ+yzUQTh/gx7wQBr2J5Hph1vZ2HkJx
+AG1ktYqoS5j3ER33yFSohBs2fJve0QMA2erMaKKx
+7kN1s2z38ZQIX5fJ/KeBn9fZs2LYpXERnht6yO5k
+xrnczm/uXSWqRmW6qGnr0TcjbngAD7dgR9iiNIiE
+sXqWKt4QOB6CuAPY7+9WbnC2/YnVe5/ZITtMemFp
+PqdSfPbRmlStaq+ND+ISb8W+4ggG3I9LVra/bqUT
+y+FT3c7U9aIHMCVcIgkNyVdqe72INuDvnr+HED0m
+VKILopGiqJbqzQmQ+dzqTiuRqcdDBSgyll2bxnnu
+vU1zGZ038HDTef2BXDd0sXgfi4pkPLlWzxrNiqyk
+oKsHwbv3dBjhWdOBrPmLVdB8vnhHP/9axVcle3bH
+FYwPsrZ1tQ80KyVJF9OGxpUcWzyrjDAyoIgOwxKu
+dnNbkkTkwgXK1h1/fSHhWuhAID9nPiectSxnpiWe
+U1ebNZyPVBj7YHi0VRGkcDQlpJx4yCIxrfhAYKge
++S0TJwpt233NNQQCDZaxYnfHNZQTR1VcJ1EGUbr6
+hl5Durgns82HU7uGO2TeFnSSSmVfli3V07CBrPuQ
+TGBLgZy2WPowK/03GoyJ/9p0vSgLYQfeUgmRw7fV
+KraJs4sUz2M67PwNdhoSPZJyOaNj+E79IHQpqoyR
+Tc9mtRDDvBlu225TCwM4D6Sjmiqsbtla8eCP3BgJ
+lOrhB4PcmChbtYMIp6BFpjDaToTgC6t91lb0mhyX
+SEvAEWcfReHTrP+cEAPtgFtQxoEKUNytW1oNfJDX
+7tSWi2J6ULTy1IpH5Rl4jKkLCMYanzabj9cwEqDs
+SsKsL7RJ6PbeapfpPfnrmvpQ34iiFaJCH0YfCTHC
+GkNH3RMKSoQAjhdbsuM0E3sO4pcLkLFPCsmoJPiu
+SkCJewvcewbaH+xxFR0uaGC2B3aBiSvy1lBYIygC
+uMVW4fYVNuJiZHJb2t7J/5mSpsDRkxlDx55KCcMk
+8eKhp7EiYV6XpIFFPElzook4KPy+Ep7m5/HFH13j
+hpmYdrrYZX5GLVikE8u9jVF4jeLvKIhnKoNF1VtI
+ro3GLVpS+77+VRW2/7dUB/F0vZDx+j3e+NXx9odC
+kutl++tAP/0zmpaUafEs7IY2O03bJw6GlSMlwb3a
+lu14FgAw9F5GeY3cXgdBvXxgkyJ8UsI5Y+mR/R2b
+m3keCl5McuK3HSeD60VLRPNlHLRxlIyWezivJBa0
+VzDJEwhoYaX/Uwv4YCe9+u9xMYqmg6acNHHMkJOB
+HnvjE2hKm4OthAmkugyuiZu/eteZLFV75IVYzQr9
+p1nRVsrwHlGv0czCWHaYj2+WkhBcRuiC+n8ylvfa
+aZgaWAnNE4wD7w1PFK7ILyZcVSovzVbq151ldCBt
+B4Yn0aQdAmr5fzkcbcXTNMSls9OMSCrQirlo9WQb
+q7MXSMLNEo3l7knqg1XZ5PpCDFIA8/Ds/8DsVI83
+wKzFvoo5Bxkq9fEpjIDKuQpIKj2KJd7/Jkk0WuTO
+crer1njeTt41flt/mpJzWmneSznfz1CVzc2TWvpX
+vBmGA+0oFf4pO0b7k2F2zdH1tJhYZZLlw4OlJmH0
+nI743I4k8cRdPYoQUDh979ZmsryGfPnapeJ/vrld
+JolTpb6smhD9BAe/g9sAb3868uczmWDS5YYGC4dh
+wHhiHl988///7777//8z8=')));
+?>
